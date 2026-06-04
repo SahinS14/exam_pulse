72e6f67bd8315b64ba43a4147c661ff746a91a54
“sahinS14” <sahinpradhan41@gmail.com>
2026-06-05 01:31:27 +0530

    Use Render backend URL

--- client/src/utils/apiBaseUrl.js
+++ client/src/utils/apiBaseUrl.js
@@ -1,6 +1,5 @@
 import Constants from "expo-constants";
 
-const configuredBaseUrl =
-  Constants.expoConfig?.extra?.apiBaseUrl || "http://10.50.243.141:5001/api";
-
-export const API_BASE_URL = configuredBaseUrl;
+export const API_BASE_URL =
+  Constants.expoConfig?.extra?.apiBaseUrl ||
+  "https://exampulse-api.onrender.com/api";
